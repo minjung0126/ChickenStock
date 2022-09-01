d9e978ca3e6adbfa744a911833d2b92539d62577
Hyeri <hyeri5739@gmail.com>
2022-09-01 12:34:06 +0900

    확인용

---
+++ src/main/java/com/chicken/project/billTax/controller/BillTaxController.java
@@ -0,0 +1,35 @@
+package com.chicken.project.billTax.controller;
+
+import com.chicken.project.billTax.model.dto.TaxBillDTO;
+import com.chicken.project.billTax.model.service.TaxBillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/billTax/*")
+public class BillTaxController {
+
+    private final TaxBillService taxbillService;
+
+    @Autowired
+    public BillTaxController(TaxBillService taxbillService) {
+        this.taxbillService = taxbillService;
+
+    }
+
+    @GetMapping("/billList")
+    public ModelAndView billList(ModelAndView mv){
+
+        List<TaxBillDTO> taxBillList = taxbillService.selectTaxBillList();
+
+        mv.addObject("taxBillList", taxBillList);
+        mv.setViewName("/billTax/billList");
+
+        return mv;
+    }
+}
--- src/main/java/com/chicken/project/billtax/model/service/TaxBillService.java
+++
@@ -1,6 +0,0 @@
-package com.chicken.project.billtax.model.service;
-
-public class TaxBillService {
-
-
-}
