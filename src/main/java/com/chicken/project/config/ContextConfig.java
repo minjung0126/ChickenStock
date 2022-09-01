0c2eb1dffeb710127b8d6a8905cfa67ab059a46c
minsukim <alstn0743@gmail.com>
2022-09-01 15:19:03 +0900

    commit

--- src/main/java/com/chicken/project/config/ContextConfig.java
+++ src/main/java/com/chicken/project/config/ContextConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan(basePackages = "com.chicken.project")
