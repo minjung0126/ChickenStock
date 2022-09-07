9b2c83d9926fcf031c735f81849e7f6cd27e52d3
Hyeri <hyeri5739@gmail.com>
2022-09-08 00:45:37 +0900

    반품리스트

--- src/main/java/com/chicken/project/reItem/controller/ReItemController.java
+++ src/main/java/com/chicken/project/reItem/controller/ReItemController.java
@@ -8,20 +8,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/reItem/*")
 public class ReItemController {
 
+    // 가맹점 반품서작성
     @GetMapping("/user/insertReItem")
     public void reItem(){
 
     }
 
+    // 가맹점 반품서 리스트
     @GetMapping("/user/storeReList")
     public void SreItem(){
 
 //        <a href="/reItem/user/reviseReItem">가맹점 반품신청 수정</a>
 
     }
+
+    // 가맹점 반품서 수정
     @GetMapping("/user/reviseReItem")
     public void RreItem(){
 
     }
+
+    @GetMapping("/admin/adminReList")
+    public void ReList(){
+
+    }
+
+    @GetMapping("/admin/adminReItem")
+    public void ReItem(){
+
+    }
+
+
 
 }
