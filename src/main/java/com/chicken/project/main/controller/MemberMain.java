db60b21c5f8f9e1cb875dd9ebb8d0a7d4ee55bd4
minsukim <alstn0743@gmail.com>
2022-09-14 15:52:57 +0900

    commit 15:52

--- src/main/java/com/chicken/project/main/controller/MemberMain.java
+++ src/main/java/com/chicken/project/main/controller/MemberMain.java
@@ -2,23 +2,35 @@ package com.chicken.project.main.controller;
 
 import com.chicken.project.member.model.dto.AdminImpl;
 import com.chicken.project.member.model.dto.StoreImpl;
+import com.chicken.project.notice.model.dto.NoticeDTO;
+import com.chicken.project.notice.model.service.NoticeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/member/*")
-public class MemberMainController {
+public class MemberMain {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final NoticeService noticeService;
 
-    @PostMapping("/moveMain")
-    public String defaultLocation(@AuthenticationPrincipal User user){
+    @Autowired
+    public MemberMain(NoticeService noticeService){
+
+        this.noticeService = noticeService;
+    }
+
+    @RequestMapping("/moveMain")
+    public ModelAndView defaultLocation(@AuthenticationPrincipal User user, ModelAndView mv){
         System.out.println("확인");
         String url = "/member/login";
 
@@ -29,18 +41,28 @@ public class MemberMainController {
 
             if(((AdminImpl)user).getEmpRoleList().get(0).getAuthCode().equals("1") || ((AdminImpl)user).getEmpRoleList().get(0).getAuthCode().equals("2")){
 
-                url = "main/admin_main";
+                List<NoticeDTO> noticeList = noticeService.selectMainNotice();
+
+                mv.addObject("noticeList", noticeList);
+                mv.setViewName("/main/admin_main");
             }
         }
 
         if(user instanceof StoreImpl){
 
             if(((StoreImpl)user).getStoreRoleList().get(0).getAuthCode().equals("3")){
+
                 System.out.println("확인용 = " + url);
-                url = "main/user_main";
+
+                List<NoticeDTO> noticeList = noticeService.selectMainNotice();
+
+                mv.addObject("noticeList", noticeList);
+                mv.setViewName("/main/user_main");
             }
         }
+//        System.out.println("((MemberImpl)user = " + ((MemberImpl)user).getEmpRoleList().get(0).getAuthCode());
 
-        return url;
+        return mv;
     }
 }
+
